83cd39d5c7bb55f6c169352fc9c7c8adea8f85f8
Vincent ARROCENA <varrocen@gmail.com>
2019-11-05 15:15:20 +0100

    Fix test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('dumb-test', function () {
   it('should always be ok', function () {
     var x = 1;
     var y = 1;
-    var dumb_result = 1;
+    var dumb_result = 2;
     var sum = x + y;
     expect(sum).to.be.equal(dumb_result);
   });
